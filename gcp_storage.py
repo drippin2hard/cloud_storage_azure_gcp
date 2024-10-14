0a42a967cef10d7bbcb1308c584ba2349c16d8ba
drippin2hard <jadonleong02@gmail.com>
2024-10-13 23:38:26 -0400

    Update gcp_storage.py

--- gcp_storage.py
+++ gcp_storage.py
@@ -4,7 +4,7 @@ import io
 import os
 
 # Step 1: Set up your Google credentials
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_secret.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_secret_3.json'
 
 # Step 2: Create a Google Cloud Storage client
 client = storage.Client()
@@ -17,6 +17,23 @@ bucket = client.bucket(bucket_name)
 image = Image.new('RGB', (100, 100), color = (73, 109, 137))
 image_byte_array = io.BytesIO()
 image.save(image_byte_array, format='PNG')
+
+
+
+files_upload = []
+for root, dirs, files in os.walk("images"):
+    for file in files:
+        files_upload.append(os.path.join(root, file))
+        
+for file in files_upload:
+    print("fWorking on {file})")
+    with open (file, 'rb') as f:
+        file_byte_array = f.read()
+print (file) 
+file = file.split("/")[-1]
+print('new file name: ', file)
+       
+print (f'File look like : {file_byte_array}')
 
 # Step 5: Upload the fake image to Google Cloud Storage
 blob = bucket.blob('fake_image.png')
